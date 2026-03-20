315f8bed86b9651559b437d44e57e01ab68254a6
liuyaoguo <liuyaoguo@bytedance.com>
2026-03-20 16:21:21 +0800

    fix: make entry importable for pyinstaller

--- picshrink/__main__.py
+++ picshrink/__main__.py
@@ -1,4 +1,10 @@
-from .app import run
+import os
+import sys
+
+if __package__ in (None, ""):
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from picshrink.app import run
 
 if __name__ == "__main__":
     run()
